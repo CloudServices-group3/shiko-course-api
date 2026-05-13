443e8302d4a1b4cda321904b8ee12214dbe89094
dana <danaamirr12@gmail.com>
2026-05-13 19:28:00 +0200

    Add DbContext, migrations and Azure SQL connection

---
+++ Data/CourseDbContext.cs
@@ -0,0 +1,11 @@
+﻿using CourseApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseApi.Data;
+
+public class CourseDbContext : DbContext
+{
+    public CourseDbContext(DbContextOptions<CourseDbContext> options) : base(options) { }
+
+    public DbSet<Course> Courses { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
+using CourseApi.Data;
 using CourseApi.Repositories;
 using CourseApi.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<CourseDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<ICourseRepository, CourseRepository>();
 builder.Services.AddScoped<ICourseService, CourseService>();
